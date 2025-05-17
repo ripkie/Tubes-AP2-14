8242c7f86999017555e8e691d651b75ee83406f3
ripkie <rifki.dupon07@gmail.com>
2025-05-17 19:35:44 +0700

    test

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+const NMAX int = 100
+
+type comment struct {
+	sentimen [NMAX]string
+}
+
 func main() {
-	fmt.Print("rifki")
+	fmt.Print("ki")
+}
+
+func InsertionSort(A *tabInt, N int) {
+	var i, pass int
+	var temp int
+
+	pass = 1
+	for pass <= N-1 {
+		i = pass
+		temp = A[pass]
+		for i > 0 && temp < A[i-1] {
+			A[i] = A[i-1]
+			i--
+		}
+		A[i] = temp
+		pass = pass + 1
+	}
 }
