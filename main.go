e13a74a844057e892ace159aa4d6eb8bcc185980
ripkie <rifki.dupon07@gmail.com>
2025-05-19 22:24:32 +0700

    add fitur login and register

--- main.go
+++ main.go
@@ -2,29 +2,79 @@ package main
 
 import "fmt"
 
-const NMAX int = 100
+type Akun struct {
+	username string
+	password string
+}
 
-type comment struct {
-	sentimen [NMAX]string
+var akunList [100]Akun
+var jumlahAkun int
+
+func register(username string, password string) string {
+	// Cek apakah username sudah ada
+	ketemu := false
+	for i := 0; i < jumlahAkun; i++ {
+		ketemu = ketemu || (akunList[i].username == username)
+	}
+	return map[bool]string{
+		true:  "Username sudah digunakan",
+		false: tambahAkun(username, password),
+	}[ketemu]
+}
+
+func tambahAkun(username string, password string) string {
+	akunList[jumlahAkun] = Akun{username, password}
+	jumlahAkun++
+	return "Registrasi berhasil"
+}
+
+func login(username string, password string) string {
+	sukses := false
+	for i := 0; i < jumlahAkun; i++ {
+		sukses = sukses || (akunList[i].username == username && akunList[i].password == password)
+	}
+	return map[bool]string{
+		true:  "Login berhasil",
+		false: "Login gagal",
+	}[sukses]
 }
 
 func main() {
-	fmt.Print("ki")
+	for {
+		var pilih int
+		fmt.Println("1. Register")
+		fmt.Println("2. Login")
+		fmt.Println("3. Keluar")
+		fmt.Print("Pilih menu: ")
+		fmt.Scan(&pilih)
+
+		pesan := map[int]string{
+			1: jalankanRegister(),
+			2: jalankanLogin(),
+			3: "Keluar",
+		}[pilih]
+
+		fmt.Println(pesan)
+		if pilih == 3 {
+			break
+		}
+	}
 }
 
-func InsertionSort(A *tabInt, N int) {
-	var i, pass int
-	var temp int
+func jalankanRegister() string {
+	var u, p string
+	fmt.Print("Username baru: ")
+	fmt.Scan(&u)
+	fmt.Print("Password baru: ")
+	fmt.Scan(&p)
+	return register(u, p)
+}
 
-	pass = 1
-	for pass <= N-1 {
-		i = pass
-		temp = A[pass]
-		for i > 0 && temp < A[i-1] {
-			A[i] = A[i-1]
-			i--
-		}
-		A[i] = temp
-		pass = pass + 1
-	}
+func jalankanLogin() string {
+	var u, p string
+	fmt.Print("Masukkan username: ")
+	fmt.Scan(&u)
+	fmt.Print("Masukkan password: ")
+	fmt.Scan(&p)
+	return login(u, p)
 }
